10343720d97776ac6c3721c30ff2ddd838d7a37e
vinz3872 <vinz3872@gmail.com>
2015-10-23 15:19:31 +0200

    modif makefile & main

--- Makefile
+++ Makefile
@@ -44,10 +44,10 @@ HEAD = 			includes/ft42.class.hpp \
 				includes/bomb.class.hpp \
 				includes/player.class.hpp \
 				includes/enemy.class.hpp \
-				includes/boss.class.hpp
-				# includes/globject.class.hpp \
-				# includes/Matrix.hpp \
-				# includes/parser.class.hpp
+				includes/boss.class.hpp \
+				includes/globject.class.hpp \
+				includes/Matrix.hpp \
+				includes/parser.class.hpp
 
 SRC = 			src/ft42.class.cpp \
 				src/entity.class.cpp \
@@ -58,10 +58,10 @@ SRC = 			src/ft42.class.cpp \
 				src/fire.class.cpp \
 				src/player.class.cpp \
 				src/enemy.class.cpp \
-				src/boss.class.cpp
-				# src/globject.class.cpp \
-				# src/Matrix.cpp \
-				# src/parser.class.cpp
+				src/boss.class.cpp \
+				src/globject.class.cpp \
+				src/Matrix.cpp \
+				src/parser.class.cpp
 
 OBJ = 			$(SRC:.cpp=.cpp.o)
 
--- src/main.cpp
+++ src/main.cpp
@@ -21,38 +21,38 @@ int main( int ac, char **av ) {
 		return (EXIT_FAILURE);
 	}
 
-// 	srand(clock());
-// 	globject::init();
-// 	Entity map[20][20];
+	srand(clock());
+	globject::init();
+	Entity map[20][20];
 
-// 	for (int i = 0; i < 20; i++)
-// 	{
-// 		for (int j = 0; j < 20; j++)
-// 		{
-// 			map[i][j].id = WALL;
-// 			if (!(rand() % 2))
-// 				map[i][j].id = 10;
-// 		}
-// 	}
-// 	std::list<Player*> lol;
-// 	int e;
-// 	static float time = 0;
-// 	for (int i = 0; i < 30; i++)
-// 	{
-// 		std::cout << globject::_object[WALL].parser._textID[i] << std::endl;
-// 	}
-// 	while (1)
-// 	{
-// //		std::cout << (1 / (clock() - time)) * CLOCKS_PER_SEC << std::endl;
-// 		time = clock();
-// 		SDL_PumpEvents();
-// 		const Uint8 *state = SDL_GetKeyboardState(NULL);
-// 		if (state[SDL_SCANCODE_RETURN]) {
-// 			exit(0);
-// 			globject::resize(1000, 1000);
-// 		}
-// 		globject::render_all(map, lol);
-// 	}
+	for (int i = 0; i < 20; i++)
+	{
+		for (int j = 0; j < 20; j++)
+		{
+			map[i][j].id = WALL;
+			if (!(rand() % 2))
+				map[i][j].id = 10;
+		}
+	}
+	std::list<Player*> lol;
+	int e;
+	static float time = 0;
+	for (int i = 0; i < 30; i++)
+	{
+		std::cout << globject::_object[WALL].parser._textID[i] << std::endl;
+	}
+	while (1)
+	{
+//		std::cout << (1 / (clock() - time)) * CLOCKS_PER_SEC << std::endl;
+		time = clock();
+		SDL_PumpEvents();
+		const Uint8 *state = SDL_GetKeyboardState(NULL);
+		if (state[SDL_SCANCODE_RETURN]) {
+			exit(0);
+			globject::resize(1000, 1000);
+		}
+		globject::render_all(map, lol);
+	}
     //Test graphique...
 	return (EXIT_SUCCESS);
 }
