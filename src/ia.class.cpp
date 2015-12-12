df3b324306754faa9919160d140db8d0863e5d38
adjivas <adjivas@users.noreply.github.com>
2015-12-12 18:19:31 +0100

    fast add of security

--- src/ia.class.cpp
+++ src/ia.class.cpp
@@ -22,21 +22,75 @@ Ia::~Ia( void ) {
 }
 
 void 	Ia::start( int time ) {
-	(void)time;
-
   std::list<Entity *>::iterator it = main_event->char_list.begin();
   std::list<Entity *>::iterator end = main_event->char_list.end();
+  int direction;
 
+  (void)time;
+  printf("start\n");
   while (it != end) {
+    printf("\tscope\n");
     if ((*it)->type == ENEMY) {
-      if (time % 2 == 1) {
-        (*it)->move(DIR_BOTTOM);
+      /// Up
+      if ((*it)->dir == DIR_UP) {
+        direction = (*it)->check_move((*it)->pos_x, (*it)->pos_y - 0.08f);
+        if (direction == EMPTY) {
+          (*it)->move(DIR_UP);
+        }
+        else {
+          if (direction == WALL) {
+            (*it)->put_bomb(BOMB_SEC_3, (*it)->pos_x, (*it)->pos_y, BOMB, (*it)->blast_radius);
+          }
+          (*it)->dir = DIR_RIGHT;
+        }
       }
-      else {
-        (*it)->move(DIR_UP);
+
+      /// Right
+
+      if ((*it)->dir == DIR_RIGHT) {
+        direction = (*it)->check_move((*it)->pos_x + 0.08f*3, (*it)->pos_y);
+        if (direction == EMPTY) {
+          (*it)->move(DIR_RIGHT);
+        }
+        else {
+          if (direction == WALL) {
+            (*it)->put_bomb(BOMB_SEC_3, (*it)->pos_x, (*it)->pos_y, BOMB, (*it)->blast_radius);
+          }
+          (*it)->dir = DIR_BOTTOM;
+        }
       }
-      break ;
+
+      /// Bottom
+      if ((*it)->dir == DIR_BOTTOM) {
+        direction = (*it)->check_move((*it)->pos_x, (*it)->pos_y + 0.08f);
+        if (direction == EMPTY) {
+          (*it)->move(DIR_BOTTOM);
+        }
+        else {
+          if (direction == WALL) {
+            (*it)->put_bomb(BOMB_SEC_3, (*it)->pos_x, (*it)->pos_y, BOMB, (*it)->blast_radius);
+          }
+          (*it)->dir = DIR_LEFT;
+        }
+      }
+
+      /// Left
+      if ((*it)->dir == DIR_LEFT) {
+        direction = (*it)->check_move((*it)->pos_x - 0.08f*3, (*it)->pos_y);
+
+        if (direction == EMPTY) {
+          (*it)->move(DIR_LEFT);
+        }
+        else {
+          if (direction == WALL) {
+            (*it)->put_bomb(BOMB_SEC_3, (*it)->pos_x, (*it)->pos_y, BOMB, (*it)->blast_radius);
+          }
+          (*it)->dir = DIR_UP;
+        }
+      }
     }
     it++;
   }
 }
+
+//printf("%f-%f\n", (*it)->pos_x, (*it)->pos_y);
